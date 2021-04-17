515ae55f729d5870e185fac8492f7771e22e4f4d
noelmurphy33 <noelmurphy33@gmail.com>
2021-04-17 15:53:34 +0000

    [ADD] Added function to toggle to how to play div

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+//function to toggle to how to play div
+$("#howtobtn").click(function () {
+    $("#intro").hide("slow");
+    $(".nav-link").removeClass("active");
+    $("#mainscoreboard").hide("slow");
+    $("#contact").hide("slow");
+    $("#howtoplay").show("slow");
+     $(this).addClass("active");
+});
