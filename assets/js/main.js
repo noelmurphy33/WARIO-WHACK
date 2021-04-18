8442e5753a0be605d2a5c2dd53dccd59fd92c5de
noelmurphy33 <noelmurphy33@gmail.com>
2021-04-18 22:48:38 +0000

    [ADD] Added func to select random pipe

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,60 @@
-$( document ).ready(function() {
-//function to toggle to how to play div
-$("#howtobtn").click(function () {
+$(document).ready(function () {
+  //function to toggle to how to play div
+  $("#howtobtn").click(function () {
     $("#intro").hide("slow");
     $(".nav-link").removeClass("active");
     $("#mainscoreboard").hide("slow");
     $("#contact").hide("slow");
     $("#howtoplay").show("slow");
-     $(this).addClass("active");
-});
-//function to toggle to scoreboard div
-$("#scoreboardbtn").click(function () {
+    $(this).addClass("active");
+  });
+  //function to toggle to scoreboard div
+  $("#scoreboardbtn").click(function () {
     $("#intro").hide("slow");
     $(".nav-link").removeClass("active");
     $("#howtoplay").hide("slow");
     $("#contact").hide("slow");
     $("#mainscoreboard").show("slow");
-     $(this).addClass("active");
-});
-//function to toggle to contact
-$("#contactbtn").click(function () {
+    $(this).addClass("active");
+  });
+  //function to toggle to contact
+  $("#contactbtn").click(function () {
     $("#intro").hide("slow");
     $(".nav-link").removeClass("active");
     $("#howtoplay").hide("slow");
     $("#mainscoreboard").hide("slow");
     $("#contact").show("slow");
-     $(this).addClass("active");
-});
-//function to toggle to intro
-$("#introbtn").click(function () {
+    $(this).addClass("active");
+  });
+  //function to toggle to intro
+  $("#introbtn").click(function () {
     $("#contact").hide("slow");
     $(".nav-link").removeClass("active");
     $("#howtoplay").hide("slow");
     $("#mainscoreboard").hide("slow");
     $("#intro").show("slow");
-     $(this).addClass("active");
-});
+    $(this).addClass("active");
+  });
 
-//gameplay 
-const pipes = document.querySelectorAll(".pipe");
-const wario = document.querySelectorAll(".wario");
-const mario = document.querySelectorAll(".mario");
-const startgame = document.querySelector(".startbtn")
-const displaytimeleft  = document.querySelector("#time")
-const displayscore  = document.querySelector("#score")
-
+  //gameplay
+  const pipes = document.querySelectorAll(".pipe");
+  const wario = document.querySelectorAll(".wario");
+  const mario = document.querySelectorAll(".mario");
+  const startgame = document.querySelector(".startbtn");
+  const displaytimeleft = document.querySelector("#time");
+  const displayscore = document.querySelector("#score");
+  let lastPipe;
+  
+  function selectRandomPipe(pipes) {
+    const randomPipe = Math.floor(Math.random() * pipes.length);
+    const pipe = pipes[randomPipe];
+    if (pipe === lastPipe) {
+      return selectRandomPipe(pipes);
+    }
+    lastPipe = pipe;
+    return pipe;
+  };
+console.log(selectRandomPipe(pipes))
 
 
 
