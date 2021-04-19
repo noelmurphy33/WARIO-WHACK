b27e6533bf1211081e5f2a9787a13f3e516b150d
noelmurphy33 <noelmurphy33@gmail.com>
2021-04-19 18:41:44 +0000

    [ADD] Added email js

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,17 +44,16 @@ $(document).ready(function () {
   const startGameBtn = document.querySelector(".startbtn");
   const displayTimeLeft = document.querySelector("#time");
   const displayScore = document.querySelector("#score");
-  const replay = document.querySelector("#playagainbtn")
+  const replay = document.querySelector("#playagainbtn");
   let lastPipe;
   let timeUp = false;
   let score = 0;
   let countDown;
   let timeLimit = 60000;
   //audio
-  const mutePlay = document.querySelector("#mutebtn")
+  const mutePlay = document.querySelector("#mutebtn");
   const marioHit = new Audio("assets/audio/hitmario.mp3");
   const warioHit = new Audio("assets/audio/hitwario.mp3");
-
 
   function selectRandomPipe(pipes) {
     const randomPipe = Math.floor(Math.random() * pipes.length);
@@ -65,7 +64,6 @@ $(document).ready(function () {
     lastPipe = pipe;
     return pipe;
   }
-  console.log(selectRandomPipe(pipes));
 
   function jumpOut() {
     const jumpOutTime = Math.random() * 1200 + 600;
@@ -81,7 +79,7 @@ $(document).ready(function () {
   }
   //start the game
   function startGame() {
-    countDown = timeLimit/1000;
+    countDown = timeLimit / 1000;
     displayScore.textContent = 0;
     displayTimeLeft.textContent = countDown;
     timeUp = false;
@@ -97,55 +95,71 @@ $(document).ready(function () {
       countDown -= 1;
       displayTimeLeft.textContent = countDown;
       if (countDown < 1) {
-          countDown = 0;
+        countDown = 0;
         clearInterval(countTimeDown);
-        gameOver.style.display = "block"
+        gameOver.style.display = "block";
       }
     }, 1000);
   }
 
   //increase score for hit wario and play sound
   function warioWhack(e) {
-    if(mutePlay.classList.contains("fa-volume-up")){
-      warioHit.play()
+    if (mutePlay.classList.contains("fa-volume-up")) {
+      warioHit.play();
     }
     score++;
     displayScore.textContent = score;
     this.parentElement.classList.remove("wariojump");
   }
-//decrease score for hit mario and play sound
+  //decrease score for hit mario and play sound
   function marioWhack(e) {
-    if(mutePlay.classList.contains("fa-volume-up")){
-      marioHit.play()
+    if (mutePlay.classList.contains("fa-volume-up")) {
+      marioHit.play();
     }
-    if(score <= 0){
+    if (score <= 0) {
       score = 0;
       displayScore.textContent = score;
-    }else
-    score--;
+    } else score--;
     displayScore.textContent = score;
     this.parentElement.classList.remove("mariojump");
   }
   // toggle mute icon
-  function music(){
-    if(mutePlay.classList.contains("fa-volume-mute")){
-    mutePlay.classList.remove("fa-volume-mute");
-    mutePlay.classList.add("fa-volume-up");
+  function music() {
+    if (mutePlay.classList.contains("fa-volume-mute")) {
+      mutePlay.classList.remove("fa-volume-mute");
+      mutePlay.classList.add("fa-volume-up");
+    } else {
+      mutePlay.classList.remove("fa-volume-up");
+      mutePlay.classList.add("fa-volume-mute");
+    }
   }
-    else{
-    mutePlay.classList.remove("fa-volume-up");
-    mutePlay.classList.add("fa-volume-mute");
+  // to play again
+  function playAgain() {
+    gameOver.style.display = "none";
+    startGame();
   }
-  }
-// to play again
-  function playAgain(){
-    gameOver.style.display = "none"
-    startGame()
-  }
+  //emailjs
+  const form = document.querySelector("#contactform");
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    emailjs.sendForm("service_wn3bigg", "wario_email", form).then(
+      function () {
+        console.log("SUCCESS!");
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+      )
+    })
+
+  
+
+  
+
   //event listener
-  replay.addEventListener("click", playAgain)
+  replay.addEventListener("click", playAgain);
   mutePlay.addEventListener("click", music);
-  wario.forEach(wario => wario.addEventListener("click", warioWhack));
-  mario.forEach(mario => mario.addEventListener("click", marioWhack));
+  wario.forEach((wario) => wario.addEventListener("click", warioWhack));
+  mario.forEach((mario) => mario.addEventListener("click", marioWhack));
   startGameBtn.addEventListener("click", startGame);
 });
